4f40cd9ad2ef06c954bfcdc3e8a760ccb80a7f40
JungHyun <f7817455@naver.com>
2021-02-04 17:15:35 +0900

    더하기 함수 구현

--- test.c
+++ test.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+int add(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
 	//this is test
 	//is conflict?
 	//conflict test
 	//hello?
+	int a, b = 0;
+
+	add(a, b);
 }
 
