c508047b57ef813eaa09e47dd4da4f11ca0968d5
JungHyun <f7817455@naver.com>
2021-02-04 17:17:09 +0900

    빼기 구현

--- test.c
+++ test.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+int substact(int a, int b)
+{
+	return a - b;
+}
+
 int main()
 {
 	//this is test
-	//¼öÁ¤
 	//is conflict?
 	//conflict test
+	int num1, num2 = 0;
+
+	substact(num1, num2);
 }
 
